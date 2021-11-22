adff66221664952dedcb3deb6cec7b6404eeee45
fangzhiyan <fangzhiyan@meituan.com>
2021-11-22 15:02:04 +0800

    re-structure and add my own files

---
+++ DataHandler.py
@@ -0,0 +1,307 @@
+import xlrd
+import math
+import pandas as pd
+import json
+from ortools.linear_solver import pywraplp
+
+# data loading
+datapath = 'SC-Form.xls'
+wb = xlrd.open_workbook(datapath)
+sheet = wb.sheet_by_index(0)
+
+stage_list = []
+for stage_index in range (1, sheet.nrows):
+	stage_dict = {'StageId': int(sheet.cell_value(stage_index,0)),\
+					'StageName': sheet.cell_value(stage_index,1),\
+					'StageType': sheet.cell_value(stage_index,1).split('_')[0],\
+					'RelDepth': int(sheet.cell_value(stage_index,2)),\
+					'StageCost': float(sheet.cell_value(stage_index,3)),\
+					'HoldingCost': float(sheet.cell_value(stage_index,3)),\
+					'avgDemand': str(sheet.cell_value(stage_index,4)),\
+					'stdDevDemand': str(sheet.cell_value(stage_index,5)),\
+					'maxServiceTime': str(sheet.cell_value(stage_index,6)),\
+					'ServiceLevel': str(sheet.cell_value(stage_index,7)),\
+					'StageTime': str(sheet.cell_value(stage_index,8)).split(';'),\
+					'DownstreamStage': str(sheet.cell_value(stage_index,9)),\
+					'UpstreamStage': str(sheet.cell_value(stage_index,10))}
+	if (len(stage_dict['avgDemand']) > 0):
+		stage_dict['avgDemand'] = float(stage_dict['avgDemand'])
+	else:
+		stage_dict['avgDemand'] = 0
+	if (len(stage_dict['stdDevDemand']) > 0):
+		stage_dict['stdDevDemand'] = float(stage_dict['stdDevDemand'])
+	else:
+		stage_dict['stdDevDemand'] = 0
+	if (len(stage_dict['maxServiceTime']) > 0):
+		stage_dict['maxServiceTime'] = float(stage_dict['maxServiceTime'])
+	else:
+		stage_dict['maxServiceTime'] = None
+	if (len(stage_dict['ServiceLevel']) > 0):
+		stage_dict['ServiceLevel'] = float(stage_dict['ServiceLevel'])
+	else:
+		stage_dict['ServiceLevel'] = 0.95
+	for i in range (len(stage_dict['StageTime'])):
+		stage_dict['StageTime'][i] = list(map(float,stage_dict['StageTime'][i].split(',')))
+	if (len(stage_dict['DownstreamStage']) > 0):
+		stage_dict['DownstreamStage'] = list(map(int,list(map(float,stage_dict['DownstreamStage'].split(',')))))
+	else:
+		stage_dict['DownstreamStage'] = None
+	if (len(stage_dict['UpstreamStage']) > 0):
+		stage_dict['UpstreamStage'] = list(map(int,list(map(float,stage_dict['UpstreamStage'].split(',')))))
+	else:
+		stage_dict['UpstreamStage'] = None
+	stage_list.append(stage_dict)
+#print(stage_list)
+
+# constants
+N = len(stage_list) #number of stages
+epslion_percent = 0.02
+MAX_ITE = 1000
+M = 100
+
+# calculate demand
+demand_cal_list = []
+for j in range (N):
+	if (stage_list[j]['UpstreamStage'] is None):
+		demand_cal_list.append(j)
+while (len(demand_cal_list) != N):
+	for j in range (N):
+		if ((stage_list[j]['avgDemand'] == 0) or (j in demand_cal_list)):
+			continue
+		cost_per_sum = 0
+		for i in stage_list[j]['UpstreamStage']:
+			if (stage_list[i]['StageType'] == 'Manuf'):
+				cost_per_sum += 1/stage_list[i]['StageCost']
+		for i in stage_list[j]['UpstreamStage']:
+			if (stage_list[i]['StageType'] != 'Manuf'):
+				stage_list[i]['avgDemand'] += stage_list[j]['avgDemand']
+				stage_list[i]['stdDevDemand'] = math.sqrt(stage_list[i]['stdDevDemand']**2+stage_list[j]['stdDevDemand']**2)
+			else:
+				stage_list[i]['avgDemand'] += stage_list[j]['avgDemand']*(1/stage_list[i]['StageCost']/cost_per_sum)
+				stage_list[i]['stdDevDemand'] = math.sqrt(stage_list[i]['stdDevDemand']**2\
+					+(stage_list[j]['stdDevDemand']*((1/stage_list[i]['StageCost']/cost_per_sum)**2))**2)
+		demand_cal_list.append(j)
+
+# calculate holding cost
+max_depth = 0
+for j in range (N):
+	if (stage_list[j]['RelDepth'] > max_depth):
+		max_depth = stage_list[j]['RelDepth']
+current_depth = max_depth - 1
+while (current_depth >= 0):
+	for j in range (N):
+		if ((stage_list[j]['RelDepth'] != current_depth) or (stage_list[j]['UpstreamStage'] is None)):
+			continue
+		cost_per_sum = 0
+		for i in stage_list[j]['UpstreamStage']:
+			if (stage_list[i]['StageType'] == 'Manuf'):
+				cost_per_sum += 1/stage_list[i]['StageCost']
+		for i in stage_list[j]['UpstreamStage']:
+			if (stage_list[i]['StageType'] != 'Manuf'):
+				stage_list[j]['HoldingCost'] += stage_list[i]['HoldingCost']
+			else:
+				stage_list[j]['HoldingCost'] += stage_list[i]['StageCost']*(1/stage_list[i]['StageCost']/cost_per_sum)
+	current_depth -= 1
+for j in range (N):
+	if (stage_list[j]['StageType'] == 'Part'):
+		stage_list[j]['HoldingCost'] = stage_list[j]['HoldingCost']*0.05
+	elif ((stage_list[j]['StageType'] == 'Manuf') or (stage_list[j]['StageType'] == 'Trans')):
+		stage_list[j]['HoldingCost'] = stage_list[j]['HoldingCost']*0.1
+	elif ((stage_list[j]['StageType'] == 'Retail') or (stage_list[j]['StageType'] == 'Dist')):
+		stage_list[j]['HoldingCost'] = stage_list[j]['HoldingCost']*0.2
+#print(stage_list)
+
+#define objective function
+def Phi(j, value):
+	phi_sum_t = 0
+	for t in range (len(stage_list[j]['StageTime'])):
+		phi_sum_t += stage_list[j]['ServiceLevel']*stage_list[j]['stdDevDemand']*stage_list[j]['StageTime'][t][1]\
+									*math.sqrt(value+stage_list[j]['StageTime'][t][0])
+	return phi_sum_t
+
+# intialization
+f_list = [] # element j = [f_r^{j,k}]
+alpha_list = [] # element j = [alpha_r^{j,k}]
+M_list = [] #element j = [M_r^j]
+R = [] #element j = R_j
+for j in range (N):
+	x_lower_bound = float('inf')
+	for t in range (len(stage_list[j]['StageTime'])):
+		if (stage_list[j]['StageTime'][t][0] < x_lower_bound):
+			x_lower_bound = stage_list[j]['StageTime'][t][0]
+	f_list.append([Phi(j,-x_lower_bound), Phi(j,M)])
+	alpha_list.append([(Phi(j,M)-Phi(j,-x_lower_bound))/(M+x_lower_bound), 0])
+	M_list.append([-x_lower_bound, M])
+	R.append(2)
+# iteration
+iteration_index = 1
+while (1):
+	# optimization
+	solver = pywraplp.Solver.CreateSolver('SCIP')
+	# variables
+	u = []
+	z = []
+	X = []
+	SI = []
+	S = []
+	for j in range (N):
+		u_r = []
+		z_r = []
+		for r in range (R[j]-1):
+			u_r.append(solver.IntVar(0.0, solver.infinity(), 'u'+str(j)+str(r)))
+			z_r.append(solver.NumVar(-solver.infinity(), solver.infinity(), 'z'+str(j)+str(r)))
+			solver.Add(u_r[r] <= 1)
+		u.append(u_r)
+		z.append(z_r)
+	for j in range (N):
+		X.append(solver.NumVar(-solver.infinity(), solver.infinity(), 'X'+str(j)))
+		SI.append(solver.NumVar(0.0, solver.infinity(), 'SI'+str(j)))
+		S.append(solver.NumVar(0.0, solver.infinity(), 'S'+str(j)))
+	# constraints
+	for j in range(N):
+		for t in range (len(stage_list[j]['StageTime'])):
+			solver.Add(X[j] + stage_list[j]['StageTime'][t][0] >= 0)
+		sum_z = 0
+		sum_u = 0
+		for r in range (R[j]-1):
+			sum_z += z[j][r]
+			sum_u += u[j][r]
+		solver.Add(sum_z == X[j])
+		solver.Add(sum_u == 1)
+		solver.Add(X[j] == SI[j] - S[j])
+		if (stage_list[j]['DownstreamStage'] is not None):
+			for i in stage_list[j]['DownstreamStage']:
+				solver.Add(SI[i] - S[j] >= 0)
+		if (stage_list[j]['UpstreamStage'] is None):
+			solver.Add(SI[j] == 0)
+		for r in range (R[j]-1):
+			solver.Add(z[j][r] <= M_list[j][r+1] * u[j][r])
+			solver.Add(z[j][r] >= M_list[j][r] * u[j][r])
+		if (stage_list[j]['maxServiceTime'] is not None):
+			solver.Add(S[j] <= stage_list[j]['maxServiceTime'])
+	# objective
+	obj = 0
+	for j in range (N):
+		for r in range (R[j]-1):
+			obj += stage_list[j]['HoldingCost']*(f_list[j][r]*u[j][r]+alpha_list[j][r]*z[j][r]-alpha_list[j][r]*M_list[j][0]*u[j][r])
+	solver.Minimize(obj)
+	# solve the problem
+	status = solver.Solve()
+	print(solver.Objective().Value())
+	if status == pywraplp.Solver.OPTIMAL:
+		print('Solution:')
+		print('Objective value =', solver.Objective().Value())
+	else:
+		print('The problem does not have an optimal solution.')
+		print('\nAdvanced usage:')
+		print('Problem solved in %f milliseconds' % solver.wall_time())
+		print('Problem solved in %d iterations' % solver.iterations())
+		print('Problem solved in %d branch-and-bound nodes' % solver.nodes())
+
+	# termination criterion
+	flag = 0
+	phi_sum = 0
+	for j in range (N):
+		phi_sum += stage_list[j]['HoldingCost']*Phi(j,round(X[j].solution_value(),2))
+	for j in range (N):
+		phi_temp = 0
+		for r in range (R[j]-1):
+			phi_temp += f_list[j][r]*u[j][r].solution_value()+alpha_list[j][r]*z[j][r].solution_value()-\
+									alpha_list[j][r]*M_list[j][0]*u[j][r].solution_value()
+		if ((stage_list[j]['HoldingCost']*abs(Phi(j,round(X[j].solution_value(),2))-phi_temp)) > (epslion_percent*phi_sum/N)):
+			flag = 1
+			break
+	if ((flag == 0) or (iteration_index >= MAX_ITE)):
+		break
+	
+	#update R, f, alpha, M for each j
+	for j in range (N):
+		n = R[j]
+		for n_temp in range (R[j]-1):
+			if ((round(X[j].solution_value(),2) >= M_list[j][n_temp]) and (round(X[j].solution_value(),2) < M_list[j][n_temp+1])):
+				n = n_temp
+				break
+		if (n == R[j]):
+			print("the pre-determined M is too small!")
+		if (round(X[j].solution_value(),2) == M_list[j][n]):
+			continue
+		
+		alpha_list[j].append(alpha_list[j][R[j]-1])
+		f_list[j].append(f_list[j][R[j]-1])
+		for n_temp in range (R[j]-n-2):
+			alpha_list[j][R[j]-n_temp-1] = alpha_list[j][R[j]-n_temp-2]
+			f_list[j][R[j]-n_temp-1] = f_list[j][R[j]-n_temp-2]
+		alpha_list[j][n] = (Phi(j,round(X[j].solution_value(),2))-Phi(j,M_list[j][n]))/(round(X[j].solution_value(),2)-M_list[j][n])
+		f_list[j][n] = Phi(j,round(X[j].solution_value(),2))-alpha_list[j][n]*(round(X[j].solution_value(),2)-M_list[j][0])
+		alpha_list[j][n+1] = (Phi(j,M_list[j][n+1])-Phi(j,round(X[j].solution_value(),2)))/(M_list[j][n+1]-round(X[j].solution_value(),2))
+		f_list[j][n+1] = Phi(j,M_list[j][n+1])-alpha_list[j][n+1]*(M_list[j][n+1]-M_list[j][0])
+		
+		R[j] += 1
+
+		M_list[j].append(0)
+		for n_temp in range (R[j]-n-2):
+			M_list[j][R[j]-n_temp-1] = M_list[j][R[j]-n_temp-2]
+		M_list[j][n+1] = round(X[j].solution_value(),2)
+
+	iteration_index += 1
+
+# output result to csv: Excel Data + SI, S, obj, real obj value, number of iterations
+for j in range (N):
+	stage_list[j]['InboundServiceTime'] = SI[j].solution_value()
+	stage_list[j]['OutboundServiceTime'] = S[j].solution_value()
+	stage_list[j]['SafetyInventoryCost'] = stage_list[j]['HoldingCost']*Phi(j,round(X[j].solution_value(),2))
+	phi_temp = 0
+	for r in range (R[j]-1):
+		phi_temp += f_list[j][r]*u[j][r].solution_value()+alpha_list[j][r]*z[j][r].solution_value()\
+								-alpha_list[j][r]*M_list[j][0]*u[j][r].solution_value()
+	stage_list[j]['ApproxObjValue'] = stage_list[j]['HoldingCost']*phi_temp
+	stage_list[j]['ObjValueGap'] = stage_list[j]['HoldingCost']*(Phi(j,round(X[j].solution_value(),2))-phi_temp)
+	stage_list[j]['IteNum'] = iteration_index
+df = pd.DataFrame(stage_list)
+df.to_csv(datapath.split('.')[0]+"Result.csv")
+
+# export to json
+jsdata = {'results': [{'columns':['user', 'entity'],'data':[{'graph': {'nodes':[],'relationships':[]}}]}],'errors':[]}
+for j in range (N):
+	if (stage_list[j]['DownstreamStage'] is None):
+		jsdata['results'][0]['data'][0]['graph']['nodes'].append({'id': str(j),
+																															'labels': [stage_list[j]['StageType']],
+																															'properties': {
+																																'Stage Name': stage_list[j]['StageName'],
+																																'Holding Cost': round(stage_list[j]['HoldingCost']),
+																																'Average Demand': round(stage_list[j]['avgDemand'],2),
+																																'Demand Std Dev.': round(stage_list[j]['stdDevDemand'],2),
+																																'Max Service Time': stage_list[j]['maxServiceTime'],
+																																'Service Level': stage_list[j]['ServiceLevel'],
+																																'Lead Time': stage_list[j]['StageTime'],
+																																'Safety Inventory Cost': round(stage_list[j]['SafetyInventoryCost'],2)
+																															}
+																															})
+	else:
+		jsdata['results'][0]['data'][0]['graph']['nodes'].append({'id': str(j),
+																															'labels': [stage_list[j]['StageType']],
+																															'properties': {
+																																'Stage Name': stage_list[j]['StageName'],
+																																'Holding Cost': round(stage_list[j]['HoldingCost']),
+																																'Average Demand': round(stage_list[j]['avgDemand'],2),
+																																'Demand Std Dev.': round(stage_list[j]['stdDevDemand'],2),
+																																'Lead Time': stage_list[j]['StageTime'],
+																																'Safety Inventory Cost': round(stage_list[j]['SafetyInventoryCost'],2)
+																															}
+																															})
+
+rela_index = 0
+for j in range (N):
+	if (stage_list[j]['DownstreamStage'] is None):
+		continue
+	for i in stage_list[j]['DownstreamStage']:
+		jsdata['results'][0]['data'][0]['graph']['relationships'].append({'id': str(rela_index),
+																	'type': str(round(stage_list[j]['OutboundServiceTime'])),
+																	'startNode': str(j),
+																	'endNode': str(i),
+																	'properties': {'Service Time': round(stage_list[j]['OutboundServiceTime'])}
+																	})
+		rela_index += 1
+
+with open('Visualization/docs/json/neo4jData.json', 'w') as json_file:
+  json.dump(jsdata, json_file, indent=2)
--- README.md
+++ README.md
@@ -1,252 +1,35 @@
-# neo4jd3.js
+# supply-chain-visualization
+![This is an image](https://github.com/FzyEstelle/supply-chain-visualization/blob/main/SC1-Snapshot.png)
+A simple web app to assign service times to supply chains based on its structure, stage costs, stage times and demand distributions, and visualize the supply chain on web locally.
 
-[Neo4j](https://github.com/neo4j) graph visualization using [D3.js](https://github.com/d3/d3).
+Input file sample: `SC1.xls` (source: Sean P. Willams, 2007: https://pubsonline.informs.org/doi/suppl/10.1287/msom.1070.0176).
 
-![neo3jd3.js](https://eisman.github.io/neo4jd3/img/neo4jd3.jpg?v=0.0.5)
+Optimization model and algorithm: Magnanti, et al., 2006 (https://www.sciencedirect.com/science/article/abs/pii/S0167637705000477).
 
-## Features
+Note that both stage time and demand can be random, which makes our model slightly different from that above.
 
-* Compaptible with the [Neo4j data format](#neo4j-data-format) and the [D3.js data format](#d3js-data-format).
-* Force simulation.
-* Info panel that shows nodes and relationships information on hover.
-* Double click callbacks.
-* Custom node colors by node type.
-* Text nodes + [Font Awesome](http://fontawesome.io/) icon nodes + SVG image nodes (e.g. using [Twitter Emoji](https://github.com/twitter/twemoji))).
-* Sticky nodes (drag to stick, single click to unstick).
-* Dynamic graph update (e.g. double click a node to expand it).
-* Highlight nodes on init.
-* Relationship auto-orientation.
-* Zoom, pan, auto fit.
-* Compatible with D3.js v4.
+### Important Notes
+1. Assume that if a stage receiving products from more than one manufacturer, all those manufacturers produce the same product;
 
-## Running
+2. It may cause faults if StageId in the input excel is not in order.
 
-First of all, make sure you have ruby and sass gem installed. Then, clone the repository, install all dependencies, build and serve the project.
+### Tutorial
+1. Download and install NodeJS;
 
-```bash
-> gem install sass
-> git clone https://github.com/eisman/neo4jd3.git
-> npm install
-> gulp
-```
+2. Download Gem on its official website and conduct environment settings;
 
-Open `http://localhost:8080` in your favorite browser.
+3. Install gulp and downgrade gulp to version 10;
 
-## Documentation
+4. Run the web app.
+   ```
+   $ cd <your downloaded supply-chain-visualization folder>
+   $ gem install sass
+   $ npm install
+   $ python3 DataHandler.py
+   $ gulp
+   ```
 
-```javascript
-var neo4jd3 = new Neo4jd3('.selector', options);
-```
+### Reference
+1. All visualization codes are based on https://github.com/eisman/neo4jd3.
 
-### Options
-
-| Parameter | Type | Description |
-| --------- | ---- | ----------- |
-| **highlight** | *array* | Highlight several nodes of the graph.<br>Example:<br>`[`<br>&nbsp;&nbsp;&nbsp;&nbsp;`{`<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`class: 'Project',`<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`property: 'name',`<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`value: 'neo4jd3'`<br>&nbsp;&nbsp;&nbsp;&nbsp;`}`<br>`]` |
-| **icons** | *object* | Map node labels to [Font Awesome icons](http://fontawesome.io/icons).<br>Example:<br>`{`<br>&nbsp;&nbsp;&nbsp;&nbsp;`'BirthDate': 'birthday-cake',`<br>&nbsp;&nbsp;&nbsp;&nbsp;`'Password': 'lock',`<br>&nbsp;&nbsp;&nbsp;&nbsp;`'Phone': 'phone',`<br>&nbsp;&nbsp;&nbsp;&nbsp;`'User': 'user'`<br>`}`. |
-| **images** | *object* | Map node labels to SVG images (e.g. using [Twitter Emoji](https://github.com/twitter/twemoji)).<br>Example:<br>`{`<br>&nbsp;&nbsp;&nbsp;&nbsp;`'Address': 'img/twemoji/1f3e0.svg',`<br>&nbsp;&nbsp;&nbsp;&nbsp;`'BirthDate': 'img/twemoji/1f382.svg',`<br>&nbsp;&nbsp;&nbsp;&nbsp;`'Password': 'img/twemoji/1f511.svg',`<br>&nbsp;&nbsp;&nbsp;&nbsp;`'Project': 'img/twemoji/2198.svg',`<br>&nbsp;&nbsp;&nbsp;&nbsp;`'Project\|name\|neo4jd3': 'img/twemoji/2196.svg',`<br>&nbsp;&nbsp;&nbsp;&nbsp;`'User': 'img/twemoji/1f600.svg'`<br>`}`. |
-| **infoPanel** | *boolean* | Show the information panel: `true`, `false`. Default: `true`. |
-| **minCollision** | *int* | Minimum distance between nodes. Default: 2 * *nodeRadius*. |
-| **neo4jData** | *object* | Graph data in [Neo4j data format](#neo4j-data-format). |
-| **neo4jDataUrl** | *string* | URL of the endpoint that serves the graph data in [Neo4j data format](#neo4j-data-format). |
-| **nodeRadius** | *int* | Radius of nodes. Default: 25. |
-| **onNodeClick** | *function* | Callback function to be executed when the user clicks a node. |
-| **onNodeDoubleClick** | *function* | Callback function to be executed when the user double clicks a node. |
-| **onNodeDragEnd** | *function* | Callback function to be executed when the user finishes dragging a node. |
-| **onNodeDragStart** | *function* | Callback function to be executed when the user starts dragging a node. |
-| **onNodeMouseEnter** | *function* | Callback function to be executed when the mouse enters a node. |
-| **onNodeMouseLeave** | *function* | Callback function to be executed when the mouse leaves a node. |
-| **onRelationshipDoubleClick** | *function* | Callback function to be executed when the user double clicks a relationship. |
-| **zoomFit** | *boolean* | Adjust the graph to the container once it has been loaded: `true`, `false`. Default: `false`. |
-
-### JavaScript API
-
-| Function | Description |
-| -------- | ----------- |
-| **appendRandomDataToNode**(*d*, *maxNodesToGenerate*) | Generates between 1 and *maxNodesToGenerate* random nodes connected to node *d* and updates the graph data. |
-| **neo4jDataToD3Data**(*data*) | Converts data from [Neo4j data format](#neo4j-data-format) to [D3.js data format](#d3js-data-format). |
-| **randomD3Data**(*d*, *maxNodesToGenerate*) | Generates between 1 and *maxNodesToGenerate* random nodes connected to node *d*. |
-| **size**() | Returns the number of nodes and relationships.<br>Example:<br>`{`<br>&nbsp;&nbsp;&nbsp;&nbsp;`nodes: 25,`<br>&nbsp;&nbsp;&nbsp;&nbsp;`relationships: 50`<br>`}` |
-| **updateWithD3Data**(*d3Data*) | Updates the graph data using the [D3.js data format](#d3js-data-format). |
-| **updateWithNeo4jData**(*neo4jData*) | Updates the graph data using the [Neo4j data format](#neo4j-data-format). |
-| **version**() | Returns the version of neo4jd3.js.<br>Example: `'0.0.1'` |
-
-### Documentation
-
-#### D3.js data format
-
-```
-{
-    "nodes": [
-        {
-            "id": "1",
-            "labels": ["User"],
-            "properties": {
-                "userId": "eisman"
-            }
-        },
-        {
-            "id": "8",
-            "labels": ["Project"],
-            "properties": {
-                "name": "neo4jd3",
-                "title": "neo4jd3.js",
-                "description": "Neo4j graph visualization using D3.js.",
-                "url": "https://eisman.github.io/neo4jd3"
-            }
-        }
-    ],
-    "relationships": [
-        {
-            "id": "7",
-            "type": "DEVELOPES",
-            "startNode": "1",
-            "endNode": "8",
-            "properties": {
-                "from": 1470002400000
-            },
-            "source": "1",
-            "target": "8",
-            "linknum": 1
-        }
-    ]
-}
-```
-
-#### Neo4j data format
-
-```
-{
-    "results": [
-        {
-            "columns": ["user", "entity"],
-            "data": [
-                {
-                    "graph": {
-                        "nodes": [
-                            {
-                                "id": "1",
-                                "labels": ["User"],
-                                "properties": {
-                                    "userId": "eisman"
-                                }
-                            },
-                            {
-                                "id": "8",
-                                "labels": ["Project"],
-                                "properties": {
-                                    "name": "neo4jd3",
-                                    "title": "neo4jd3.js",
-                                    "description": "Neo4j graph visualization using D3.js.",
-                                    "url": "https://eisman.github.io/neo4jd3"
-                                }
-                            }
-                        ],
-                        "relationships": [
-                            {
-                                "id": "7",
-                                "type": "DEVELOPES",
-                                "startNode": "1",
-                                "endNode": "8",
-                                "properties": {
-                                    "from": 1470002400000
-                                }
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
-    ],
-    "errors": []
-}
-```
-
-### Example
-
-Live example @ [https://eisman.github.io/neo4jd3/](https://eisman.github.io/neo4jd3/)
-
-```javascript
-var neo4jd3 = new Neo4jd3('#neo4jd3', {
-    highlight: [
-        {
-            class: 'Project',
-            property: 'name',
-            value: 'neo4jd3'
-        }, {
-            class: 'User',
-            property: 'userId',
-            value: 'eisman'
-        }
-    ],
-    icons: {
-        'Api': 'gear',
-        'BirthDate': 'birthday-cake',
-        'Cookie': 'paw',
-        'Email': 'at',
-        'Git': 'git',
-        'Github': 'github',
-        'Ip': 'map-marker',
-        'Issues': 'exclamation-circle',
-        'Language': 'language',
-        'Options': 'sliders',
-        'Password': 'asterisk',
-        'Phone': 'phone',
-        'Project': 'folder-open',
-        'SecurityChallengeAnswer': 'commenting',
-        'User': 'user',
-        'zoomFit': 'arrows-alt',
-        'zoomIn': 'search-plus',
-        'zoomOut': 'search-minus'
-    },
-    images: {
-        'Address': 'img/twemoji/1f3e0.svg',
-        'BirthDate': 'img/twemoji/1f5d3.svg',
-        'Cookie': 'img/twemoji/1f36a.svg',
-        'CreditCard': 'img/twemoji/1f4b3.svg',
-        'Device': 'img/twemoji/1f4bb.svg',
-        'Email': 'img/twemoji/2709.svg',
-        'Git': 'img/twemoji/1f5c3.svg',
-        'Github': 'img/twemoji/1f5c4.svg',
-        'icons': 'img/twemoji/1f38f.svg',
-        'Ip': 'img/twemoji/1f4cd.svg',
-        'Issues': 'img/twemoji/1f4a9.svg',
-        'Language': 'img/twemoji/1f1f1-1f1f7.svg',
-        'Options': 'img/twemoji/2699.svg',
-        'Password': 'img/twemoji/1f511.svg',
-        'Project|name|d3': 'img/twemoji/32-20e3.svg',
-        'Project|name|neo4j': 'img/twemoji/33-20e3.svg',
-        'Project|name|neo4jd3': 'img/twemoji/31-20e3.svg',
-        'User': 'img/twemoji/1f600.svg'
-    },
-    minCollision: 60,
-    neo4jDataUrl: 'json/neo4jData.json',
-    nodeRadius: 25,
-    onNodeDoubleClick: function(node) {
-        switch(node.id) {
-            case '25':
-                // Google
-                window.open(node.properties.url, '_blank');
-                break;
-            default:
-                var maxNodes = 5,
-                    data = neo4jd3.randomD3Data(node, maxNodes);
-                neo4jd3.updateWithD3Data(data);
-                break;
-        }
-    },
-    zoomFit: true
-});
-```
-
-## What's coming?
-
-* Toolbar.
-* More than one relationship between two nodes.
-* Markers.
-* Performance optimization.
-* Testing.
-
-## Copyright and license
-
-Code and documentation copyright 2016 the author. Code released under the [MIT license](LICENSE). Docs released under [Creative Commons](docs/LICENSE).
+2. Solver: https://developers.google.com/optimization.
